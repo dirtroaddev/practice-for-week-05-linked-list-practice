fb842d253e04879e34e0d5ec2f7612f49fe68281
dirtroaddev <rongoodwindev@gmail.com>
2024-02-26 19:01:32 -0500

    Finished single linked list

--- doubly-linked-list.js
+++ doubly-linked-list.js
@@ -9,15 +9,49 @@ class DoublyLinkedListNode {
 class DoublyLinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   addToHead(val) {
     // Your code here
+    let newList = new DoublyLinkedListNode(val);
+    if(this.head == null) {
+      this.head = newList;
+      this.tail = newList
+      
+    }; 
+    newList.next = this.head;
+    this.head.prev = newList;
+    newList.next = this.head;
+    this.head = newList
+    
+    this.length += 1;
   }
 
   addToTail(val) {
     // Your code here
+    let newNode = new DoublyLinkedListNode(val);
+    
+    if(this.head == null) {
+       this.head = newNode;
+       this.tail = newNode;
+       this.length += 1;
+    } else {
+    let temp = this.head
+    while(temp.next !== null) {
+      temp = temp.next;
+    }
+
+    this.head.prev = null;
+    temp.next = newNode;
+    newNode.prev = temp;
+    this.tail = newNode;
+    this.length += 1;
   }
+
+}
 
   // You can use this function to help debug
   print() {
--- linked-list.js
+++ linked-list.js
@@ -8,14 +8,35 @@ class LinkedListNode {
 class LinkedList {
   constructor() {
     // Your code here
+    this.head = null;
+    this.length = 0;
+    
   }
 
   addToHead(val) {
     // Your code here
+    let newList = new LinkedListNode(val);
+    newList.next = this.head;
+    this.head = newList
+    this.length += 1;
   }
 
   addToTail(val) {
     // Your code here
+    let newNode = new LinkedListNode(val);
+    
+    if(this.head == null) {
+       this.head = newNode;
+       this.length += 1;
+    } else {
+    let temp = this.head
+    while(temp.next !== null) {
+      temp = temp.next;
+    }
+
+    temp.next = newNode;
+    this.length += 1;
+  }
   }
 
   // You can use this function to help debug
